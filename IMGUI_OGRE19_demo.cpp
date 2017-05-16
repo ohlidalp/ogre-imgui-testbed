ea7fa5e96c4997f9d52089d6c6d270ed2bdac6cf
Petr Ohlidal <petr@ohlidal.cz>
2017-05-16 15:47:29 +0200

    Builds and runs

--- IMGUI_OGRE19_demo.cpp
+++ IMGUI_OGRE19_demo.cpp
@@ -130,16 +130,8 @@ bool TutorialApplication::processUnbufferedInput(const Ogre::FrameEvent& fe)
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
 #endif
- 
-#ifdef __cplusplus
-extern "C" {
-#endif
- 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-  INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
-#else
-  int main(int argc, char *argv[])
-#endif
+
+int main(int argc, char *argv[])
   {
     // Create application object
     TutorialApplication app;
@@ -161,8 +153,4 @@ extern "C" {
  
     return 0;
   }
- 
-#ifdef __cplusplus
-}
-#endif
 
