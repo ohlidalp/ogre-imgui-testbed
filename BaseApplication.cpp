664d0a62bfe103bb139b8023e773bcb220b96761
Petr Ohlidal <petr@ohlidal.cz>
2017-05-19 14:03:10 +0200

    Returned to "App class" design - turns out it's cleaner with all the listeners

--- BaseApplication.cpp
+++ BaseApplication.cpp
@@ -1,4 +1,6 @@
 
+#include "stdafx.h" // Precompiled
+
 #include <OgreCamera.h>
 #include <OgreEntity.h>
 #include <OgreLogManager.h>
@@ -21,125 +23,10 @@
     static const std::string mPluginsCfg = "plugins.cfg";
 #endif
 
-static Ogre::Root*                 mRoot       = nullptr;
-static Ogre::Camera*               mCamera     = nullptr;
-static Ogre::SceneManager*         mSceneMgr   = nullptr;
-static Ogre::RenderWindow*         mWindow     = nullptr;
-
-static bool                        mShutDown = false;
-
-//OIS Input devices
-static OIS::InputManager*          mInputManager = nullptr;
-static OIS::Mouse*                 mMouse        = nullptr;
-static OIS::Keyboard*              mKeyboard     = nullptr;
-
-// Declarations
-class MiniWindowHandler;
-void Shutdown(MiniWindowHandler* win_handler);
-
-class MiniFrameListener: public Ogre::FrameListener
-{
-    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt) override
-    {
-        return (!mWindow->isClosed() && (!mShutDown)); // False means "exit the application"
-    }
-
-    virtual bool frameStarted(const Ogre::FrameEvent& evt) override
-    {
-        // Need to capture/update each device
-        mKeyboard->capture();
-        mMouse->capture();
-
-        // ===== Start IMGUI frame =====
-        Ogre::Rect screen_size(0,0,mWindow->getWidth(),mWindow->getHeight());
-        Ogre::ImguiManager::getSingleton().newFrame(evt.timeSinceLastFrame, screen_size);
-
-        // ===== Draw IMGUI demo window ====
-        ImGui::ShowTestWindow();
-
-        return true;
-    }
-};
-
-class MiniInputListener: public OIS::KeyListener, public OIS::MouseListener
-{
-    bool MiniInputListener::keyPressed( const OIS::KeyEvent &arg ) override
-    {
-        if (arg.key == OIS::KC_ESCAPE) // All extras we need
-        {
-            mShutDown = true;
-        }
-
-        Ogre::ImguiManager::getSingleton().keyPressed(arg);
-        return true;
-    }
-
-    bool MiniInputListener::keyReleased(const OIS::KeyEvent &arg) override
-    {
-        Ogre::ImguiManager::getSingleton().keyReleased(arg);
-        return true;
-    }
-
-    bool MiniInputListener::mouseMoved(const OIS::MouseEvent &arg) override
-    {
-        Ogre::ImguiManager::getSingleton().mouseMoved(arg);
-        return true;
-    }
-
-    bool MiniInputListener::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id) override
-    {
-        Ogre::ImguiManager::getSingleton().mousePressed(arg, id);
-        return true;
-    }
-
-    bool MiniInputListener::mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id) override
-    {
-        Ogre::ImguiManager::getSingleton().mouseReleased(arg, id);
-        return true;
-    }
-};
-
-class MiniWindowHandler: public Ogre::WindowEventListener
+class DemoApp: public Ogre::FrameListener, public OIS::KeyListener, public OIS::MouseListener,  public Ogre::WindowEventListener
 {
 public:
-    // Adjust mouse clipping area
-    virtual void windowResized(Ogre::RenderWindow* rw) override
-    {
-        unsigned int width, height, depth;
-        int left, top;
-        rw->getMetrics(width, height, depth, left, top);
-
-        const OIS::MouseState &ms = mMouse->getMouseState();
-        ms.width = width;
-        ms.height = height;
-    }
-
-    // Unattach OIS before window shutdown (very important under Linux)
-    virtual void windowClosed(Ogre::RenderWindow* rw) override
-    {
-        Shutdown(this); // We only have one window
-    }
-};
-
-void Shutdown(MiniWindowHandler* win_handler)
-{
-    if(mInputManager)
-    {
-        mInputManager->destroyInputObject(mMouse);
-        mInputManager->destroyInputObject(mKeyboard);
-
-        OIS::InputManager::destroyInputSystem(mInputManager);
-        mInputManager = 0;
-    }
-    // Remove ourself as a Window listener
-    Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, win_handler);
-    delete mRoot;
-    mRoot = nullptr;
-}
-
-int main(int argc, char *argv[])
-{
-    try
+    void Go()
     {
         mRoot = new Ogre::Root(mPluginsCfg);
 
@@ -149,7 +36,7 @@ int main(int argc, char *argv[])
         {
             // User abort
             delete mRoot;
-            return 0;
+            return;
         }
 
         const bool create_window = true; // Let's be descriptive :)
@@ -188,34 +75,143 @@ int main(int argc, char *argv[])
         pl.insert(OIS::ParamList::value_type("w32_mouse", "DISCL_FOREGROUND"));
         pl.insert(OIS::ParamList::value_type("w32_mouse", "DISCL_NONEXCLUSIVE"));
 #endif
-
         mInputManager = OIS::InputManager::createInputSystem(pl);
 
         mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true));
         mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject(OIS::OISMouse, true));
 
-        MiniWindowHandler window_handler;
-
         // Set initial mouse clipping size
-        window_handler.windowResized(mWindow);
+        this->windowResized(mWindow);
 
         // Register as a Window listener
-        Ogre::WindowEventUtilities::addWindowEventListener(mWindow, &window_handler);
+        Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
 
         // === Create IMGUI ====
         Ogre::ImguiManager::createSingleton();
         Ogre::ImguiManager::getSingleton().init(mSceneMgr, mKeyboard, mMouse); // OIS mouse + keyboard
 
-        MiniFrameListener frame_listener;
-        mRoot->addFrameListener(&frame_listener);
+        mRoot->addFrameListener(this);
 
-        MiniInputListener input_listener;
-        mMouse->setEventCallback(&input_listener);
-        mKeyboard->setEventCallback(&input_listener);
+        mMouse->setEventCallback(this);
+        mKeyboard->setEventCallback(this);
 
         mRoot->startRendering();
 
-        Shutdown(&window_handler);
+        this->Shutdown();
+    }
+
+private:
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt) override
+    {
+        return (!mWindow->isClosed() && (!mShutDown)); // False means "exit the application"
+    }
+
+    virtual bool frameStarted(const Ogre::FrameEvent& evt) override
+    {
+        // Need to capture/update each device
+        mKeyboard->capture();
+        mMouse->capture();
+
+        // ===== Start IMGUI frame =====
+        Ogre::Rect screen_size(0,0,mWindow->getWidth(),mWindow->getHeight());
+        Ogre::ImguiManager::getSingleton().newFrame(evt.timeSinceLastFrame, screen_size);
+
+        // ===== Draw IMGUI demo window ====
+        ImGui::ShowTestWindow();
+
+        return true;
+    }
+
+    bool keyPressed( const OIS::KeyEvent &arg ) override
+    {
+        if (arg.key == OIS::KC_ESCAPE) // All extras we need
+        {
+            mShutDown = true;
+        }
+
+        Ogre::ImguiManager::getSingleton().keyPressed(arg);
+        return true;
+    }
+
+    bool keyReleased(const OIS::KeyEvent &arg) override
+    {
+        Ogre::ImguiManager::getSingleton().keyReleased(arg);
+        return true;
+    }
+
+    bool mouseMoved(const OIS::MouseEvent &arg) override
+    {
+        Ogre::ImguiManager::getSingleton().mouseMoved(arg);
+        return true;
+    }
+
+    bool mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id) override
+    {
+        Ogre::ImguiManager::getSingleton().mousePressed(arg, id);
+        return true;
+    }
+
+    bool mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id) override
+    {
+        Ogre::ImguiManager::getSingleton().mouseReleased(arg, id);
+        return true;
+    }
+
+    // Adjust mouse clipping area
+    virtual void windowResized(Ogre::RenderWindow* rw) override
+    {
+        unsigned int width, height, depth;
+        int left, top;
+        rw->getMetrics(width, height, depth, left, top);
+
+        const OIS::MouseState &ms = mMouse->getMouseState();
+        ms.width = width;
+        ms.height = height;
+    }
+
+    virtual void windowClosed(Ogre::RenderWindow* rw) override
+    {
+        this->Shutdown(); // Unattach OIS before window shutdown (very important under Linux)
+        mShutDown = true; // Stop application
+    }
+
+    void Shutdown()
+    {
+        if(mInputManager)
+        {
+            mInputManager->destroyInputObject(mMouse);
+            mInputManager->destroyInputObject(mKeyboard);
+
+            OIS::InputManager::destroyInputSystem(mInputManager);
+            mInputManager = 0;
+        }
+        // Remove ourself as a Window listener
+        Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+        delete mRoot;
+        mRoot = nullptr;
+    }
+
+    Ogre::Root*                 mRoot    ;
+    Ogre::Camera*               mCamera  ;
+    Ogre::SceneManager*         mSceneMgr;
+    Ogre::RenderWindow*         mWindow  ;
+
+    bool                        mShutDown;
+
+    //OIS Input devices
+    OIS::InputManager*          mInputManager;
+    OIS::Mouse*                 mMouse       ;
+    OIS::Keyboard*              mKeyboard    ;
+};
+
+
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        DemoApp demo_app;
+        demo_app.Go();
     }
     catch(Ogre::Exception& e)
     {
